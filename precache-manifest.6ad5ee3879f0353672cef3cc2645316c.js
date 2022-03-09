bb05f97cbf02cb5b61a7c1066eff9a15aef737f8
Emir <dzhetybaev19emir@gmail.com>
2022-03-10 01:43:01 +0600

    init

---
+++ precache-manifest.6ad5ee3879f0353672cef3cc2645316c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d29e3d0e0922661bac35",
+    "url": "/digital-resume/css/app.04d8e8ff.css"
+  },
+  {
+    "revision": "fa392ad12c8fcb387394",
+    "url": "/digital-resume/css/chunk-vendors.263f31f1.css"
+  },
+  {
+    "revision": "07baad36c3254f8adb602fcfd5e51c16",
+    "url": "/digital-resume/index.html"
+  },
+  {
+    "revision": "d29e3d0e0922661bac35",
+    "url": "/digital-resume/js/app.ccd2d2c5.js"
+  },
+  {
+    "revision": "fa392ad12c8fcb387394",
+    "url": "/digital-resume/js/chunk-vendors.cafd5755.js"
+  },
+  {
+    "revision": "d49cf6b1baa26ed7d400b042944ed7d0",
+    "url": "/digital-resume/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/digital-resume/robots.txt"
+  }
+]);
